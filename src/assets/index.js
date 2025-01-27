ec7cd32824454c944776d501237f3be5f540c96d
Niraj Salunke <nirajsalunke07@gmail.com>
2025-01-27 14:54:49 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,12 @@
-import airbnb from "./airbnb.png";
+import airbnb from "https://hoobankniraj.netlify.app/airbnb.png";
 import bill from "./bill.png";
 import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./logo.svg";
+import logo from "https://hoobankniraj.netlify.app/logo.svg";
 import quotes from "./quotes.svg";
-import robot from "./robot.png";
+import robot from "https://hoobankniraj.netlify.app/robot.png";
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
@@ -20,9 +20,9 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
+import people01 from "https://hoobankniraj.netlify.app/people01.png";
+import people02 from "https://hoobankniraj.netlify.app/people02.png";
+import people03 from "https://hoobankniraj.netlify.app/people03.png";
 
 export {
   airbnb,
